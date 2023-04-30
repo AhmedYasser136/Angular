3b3022f43a93c58f497463b76ae1eee363cc47be
Ahmed Yasser <eng.ahmed.yasser136@gmail.com>
2023-04-30 04:22:30 +0300

    some update of proj

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -4,12 +4,12 @@ import { DiscountOffers, IProduct, ICategory } from '../shared-classes-and-types
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  // styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-  Discount: DiscountOffers = DiscountOffers['10%'];
-  StoreName: string = "A.IOT";
-  StoreLogo: string = "https://cdn3.emoji.gg/emojis/1946_anemo.png";
+  Discount: DiscountOffers = DiscountOffers['No Discount'];
+  // StoreName: string = "A.IOT";
+  // StoreLogo: string = "https://cdn3.emoji.gg/emojis/1946_anemo.png";
   ProductList: IProduct[] = [
     { ID: 1, Name: "Product 1", Quantity: 10, Price: 100, Img: "https://lampatronics.com/wp-content/uploads/2022/04/rpi_pico.jpg" },
     { ID: 2, Name: "Product 2", Quantity: 5, Price: 50, Img: "https://lampatronics.com/wp-content/uploads/2022/04/rpi_pico.jpg" },
